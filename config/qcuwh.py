a1fb3e45e5a38453d4c43bfbec909a0a6badc8d3
TouhouGO <93237754+TouhouGO@users.noreply.github.com>
2023-03-09 20:11:45 +0800

    Update qcuwh.py

--- config/qcuwh.py
+++ config/qcuwh.py
@@ -4,8 +4,8 @@ from config import _Report
 
 class QCUWHHealthReport(_Report):
     """
-    QCUWH Health Report
-    该脚本由他人贡献，本人未充分测试，不保证其可用性
+    2023/2/13
+    
     """
     def __init__(self, username, password, school_id=''):
         _Report.__init__(self, username, password, school_id)
@@ -13,7 +13,7 @@ class QCUWHHealthReport(_Report):
         self._form_id = '7185'
         self._enc = 'f837c93e0de9d9ad82db707b2c27241e'
         self._reporter_name = 'QCUWH健康表单'
-        
+
         self._day_id = -1
         self._report_time_id = -1
         self._temperature_ids = []
@@ -22,51 +22,26 @@ class QCUWHHealthReport(_Report):
         self._isShow_ids = [63]
 
 
-        """
-        56:三针是否龙泉卫生院接种
-        57:两针
-        58:一针
-        同时只出现在一个，val可能不存在
-        45选项为未接种原因，当有57或58时候才会显示
-        """
 
     def _clean_form_data(self):
         form_data = self._last_form_data
         for f in form_data:
-            if f['id'] == self._day_id:
-                # 打卡日期
-                today = self._t.today
-                if f['fields'][0]['values'][0]['val'] == today:
-                    # 如果获取到上次的打卡时间是今天的，则不需要再次填报
-                    self._result = '%s今日%s已填报过%s' % (self._username_masked, today, self._reporter_name)
-                    raise Exception(self._result)
+            if f['id'] in self._options_ids:
+                if not f['fields'][0]['values']:  # 没数据，不显示,处理56,57,58
+                    f['isShow'] = False
                 else:
-                    f['fields'][0]['values'][0]['val'] = today
-            elif f['id'] == self._report_time_id:
-                # 打卡时间
-                today = self._t.today
-                report_time = self._t.report_time
-                if f['fields'][0]['values'][0]['val'].startswith(today):
-                    # 同上
-                    self._result = '%s今日%s已填报过%s' % (self._username_masked, today, self._reporter_name)
-                    raise Exception(self._result)
-                else:
-                    f['fields'][0]['values'][0]['val'] = report_time
-            elif f['id'] in self._temperature_ids and f['id'] not in self._options_ids:
-                # 体温
-                temperature = self._random_temperature()
-                f['fields'][0]['values'][0]['val'] = temperature
-            elif f['id'] in self._options_ids and f['id'] not in self._isShow_ids:
-                # 下拉项选择改写为 true
-                for option in f['fields'][0]['options']:
-                    if f['fields'][0]['values'][0]['val'] == option['title']:
-                        option['checked'] = True
-            elif f['id'] in self._hasAuthority_ids:
-                # 内部使用的id
-                f['hasAuthority'] = False
-            elif f['id'] in self._isShow_ids:
-                # 内部使用的id
+                    # 下拉项选择改写为 true
+                    for option in f['fields'][0]['options']:
+                        if f['fields'][0]['values'][0]['val'] == option['title']:
+                            option['checked'] = True
+            elif f['id'] in self._isShow:
                 f['isShow'] = False
+            elif f['id'] in self._hasAuthority_ids:
+                f['hasAuthority'] = False
+            # 这个是文本填写,处理45
+            elif f['id'] in self._edittext_area:
+                if not f['fields'][0]['values']:  # 没数据，不显示
+                    f['isShow'] = False
 
         self._today_form_data = form_data
         return form_data
